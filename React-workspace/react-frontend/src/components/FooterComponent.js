d8c475395678e470376de8f8f52a02716619e7cb
kola dinesh <dinesh292002@gmail.com>
2024-12-27 15:16:20 +0530

    react project

---
+++ React-workspace/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+function FooterComponent() {
+  return (
+    <footer>
+        <div className='footer'>AllRight Reserved &copy; by Dinesh Kola</div>
+    </footer>
+    
+  )
+}
+
+export default FooterComponent
